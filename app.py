9673985f87dac146e983ee50aeb2d31bd1fae54b
ArtPlanning <67249900+ArtPlanning@users.noreply.github.com>
2023-01-02 15:50:12 +0700

    add main

--- app.py
+++ app.py
@@ -110,6 +110,8 @@ def page_not_found(e):
 # def internal_server_error(e):
 #     return render_template('500.html'), 500
 
+if __name__ == '__main__':
+    app.run(debug=True)
 
 bootstrap = Bootstrap(app)
 mysql = MySQL(app)
